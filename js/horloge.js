283455c6971cc5e57a6b60405f499e7e5c7a54e2
timoz <timoz974@hotmail.fr>
2017-07-08 16:10:19 +0400

    tableau coordonnées

--- js/horloge.js
+++ js/horloge.js
@@ -4,41 +4,57 @@ let compteur_heures = 0;
 let compteur_secondes = 0;
 let start;
 // variable global : selections des elements
-let position = document.querySelectorAll('div');
+
 let onOff = document.querySelectorAll('button');
 let minute = document.getElementById('aiguilleminute');
 let heure = document.getElementById('aiguilleheure');
 
+const position = [
+     "top: 130px; left: 245px;",
+     "top: 180px; left: 275px;",
+     "top: 215px; left: 290px;",
+     "top: 265px; left: 285px;",
+     "top: 300px; left: 245px;",
+     "top: 315px; left: 195px;",
+     "top: 300px; left: 145px;",
+     "top: 265px; left: 110px;",
+     "top: 215px; left: 95px;",
+     "top: 165px; left: 110px;",
+     "top: 130px; left: 145px;",
+     "top: 110px; left: 200px",
+]
+;
 function addSeconde(){
 // MINUTES
-    compteur_secondes += 20; // pour le test, sinon mettre la valeur a ++.
-    if(compteur_minutes === 5) {minute.setAttribute('style', 'top: 130px; left: 245px');}
-    if(compteur_minutes === 10){minute.setAttribute('style', 'top: 180px; left: 275px');}
-    if(compteur_minutes === 15){minute.setAttribute('style', 'top: 215px; left: 290px');}
-    if(compteur_minutes === 20){minute.setAttribute('style', 'top: 265px; left: 285px');}
-    if(compteur_minutes === 25){minute.setAttribute('style', 'top: 300px; left: 245px');}
-    if(compteur_minutes === 30){minute.setAttribute('style', 'top: 315px; left: 195px');}
-    if(compteur_minutes === 35){minute.setAttribute('style', 'top: 300px; left: 145px');}
-    if(compteur_minutes === 40){minute.setAttribute('style', 'top: 265px; left: 110px');}
-    if(compteur_minutes === 45){minute.setAttribute('style', 'top: 215px; left: 95px');}
-    if(compteur_minutes === 50){minute.setAttribute('style', 'top: 165px; left: 110px');}
-    if(compteur_minutes === 55){minute.setAttribute('style', 'top: 130px; left: 145px');}
+    compteur_secondes += 1; // pour le test, sinon mettre la valeur a ++.
+    if(compteur_minutes === 5)  {minute.setAttribute('style', position[0]);}                                 
+    if(compteur_minutes === 10) {minute.setAttribute('style', position[1]);}       
+    if(compteur_minutes === 15) {minute.setAttribute('style', position[2]);}       
+    if(compteur_minutes === 20) {minute.setAttribute('style', position[3]);}        
+    if(compteur_minutes === 25) {minute.setAttribute('style', position[4]);}       
+    if(compteur_minutes === 30) {minute.setAttribute('style', position[5]);}        
+    if(compteur_minutes === 35) {minute.setAttribute('style', position[6]);}        
+    if(compteur_minutes === 40) {minute.setAttribute('style', position[7]);}        
+    if(compteur_minutes === 45) {minute.setAttribute('style', position[8]);}        
+    if(compteur_minutes === 50) {minute.setAttribute('style', position[9]);}        
+    if(compteur_minutes === 55) {minute.setAttribute('style', position[10]);}      
+    if(compteur_minutes === 60) {minute.setAttribute('style', position[11]);}      
     
 
 // HEURES
 
-    if(compteur_heures === 1) {heure.setAttribute('style', 'top: 130px; left: 245px');}
-    if(compteur_heures === 2){heure.setAttribute('style', 'top: 180px; left: 275px');}
-    if(compteur_heures === 3){heure.setAttribute('style', 'top: 215px; left: 290px');}
-    if(compteur_heures === 4){heure.setAttribute('style', 'top: 265px; left: 285px');}
-    if(compteur_heures === 5){heure.setAttribute('style', 'top: 300px; left: 245px');}
-    if(compteur_heures === 6){heure.setAttribute('style', 'top: 315px; left: 195px');}
-    if(compteur_heures === 7){heure.setAttribute('style', 'top: 300px; left: 145px');}
-    if(compteur_heures === 8){heure.setAttribute('style', 'top: 265px; left: 110px');}
-    if(compteur_heures === 9){heure.setAttribute('style', 'top: 215px; left: 95px');}
-    if(compteur_heures === 10){heure.setAttribute('style', 'top: 165px; left: 110px');}
-    if(compteur_heures === 11){heure.setAttribute('style', 'top: 130px; left: 145px');}
-    if(compteur_heures === 12){heure.setAttribute('style', 'top: 110px; left: 200px');}
+    if(compteur_heures === 1)  {heure.setAttribute('style', 'top: 130px; left: 245px;');}
+    if(compteur_heures === 2)  {heure.setAttribute('style', 'top: 180px; left: 275px');}
+    if(compteur_heures === 3)  {heure.setAttribute('style', 'top: 215px; left: 290px');}
+    if(compteur_heures === 4)  {heure.setAttribute('style', 'top: 265px; left: 285px');}
+    if(compteur_heures === 5)  {heure.setAttribute('style', 'top: 300px; left: 245px');}
+    if(compteur_heures === 6)  {heure.setAttribute('style', 'top: 315px; left: 195px');}
+    if(compteur_heures === 7)  {heure.setAttribute('style', 'top: 300px; left: 145px');}
+    if(compteur_heures === 8)  {heure.setAttribute('style', 'top: 265px; left: 110px');}
+    if(compteur_heures === 9)  {heure.setAttribute('style', 'top: 215px; left: 95px');}
+    if(compteur_heures === 10) {heure.setAttribute('style', 'top: 165px; left: 110px');}
+    if(compteur_heures === 11) {heure.setAttribute('style', 'top: 130px; left: 145px');}
+    if(compteur_heures === 12) {heure.setAttribute('style', 'top: 110px; left: 200px');}
 
     if(compteur_secondes === 60){
         compteur_minutes ++;
@@ -61,7 +77,7 @@ function addSeconde(){
     }
 
     
-    console.log(compteur_secondes + ' seconde : ' + compteur_minutes + ' minute : ' + compteur_heures + ' heure');
+    //console.log(compteur_secondes + ' seconde : ' + compteur_minutes + ' minute : ' + compteur_heures + ' heure');
 }
 
 onOff[0].addEventListener('click', ()=> {
